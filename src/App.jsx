b80a58b9cce712b6822ae17e236e5d67b44b7c5f
Daniel Iglesias <dev.daif@gmail.com>
2023-06-21 09:57:34 -0400

    added date picker test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import './App.css'
 import UserGrid from './components/UserGrid'
 import UserTable from './components/UserTable/index'
+import styled from '@mui/styled-engine'
+
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
+import { DatePicker } from '@mui/x-date-pickers'
+
+const StyledDatePicker = styled(DatePicker)(({ theme }) => ({
+  '& > .MuiInputBase-root': {
+    background: 'lime',
+    border: '3px solid red'
+  }
+}))
 
 function App() {
   return (
     <>
       <UserTable />
       <UserGrid />
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <StyledDatePicker />
+      </LocalizationProvider>
     </>
   )
 }
